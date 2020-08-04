d8ef3a9f7896bf991df3e3107c640cab77d3b0de
DenisuDevForFood <denisfeier98@yahoo.com>
2020-08-04 16:33:59 +0300

    Ignorer implemented

---
+++ src/main/java/com/denisfeier/globIgnorer/Ignorer.java
@@ -0,0 +1,37 @@
+package com.denisfeier.globIgnorer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.util.List;
+
+public class Ignorer {
+
+    private final List<PathMatcher> blackMatchersGlobs;
+    private final List<PathMatcher> whiteMatchersGlobs;
+
+    private final Logger logger = LoggerFactory.getLogger(Ignorer.class);
+
+    public Ignorer(List<PathMatcher> blackMatchersGlobs, List<PathMatcher> whiteMatchersGlobs) {
+        this.blackMatchersGlobs = blackMatchersGlobs;
+        this.whiteMatchersGlobs = whiteMatchersGlobs;
+    }
+
+    public boolean accept(String path) {
+        boolean whiteGlobs = this.match(this.whiteMatchersGlobs, path);
+        boolean blackGlobs = this.match(this.blackMatchersGlobs, path);
+
+        this.logger.info("Black Globs res: {}", blackGlobs);
+        this.logger.info("White Globs res: {}", whiteGlobs);
+        this.logger.info("Match Path {} with globs", path);
+
+        return whiteGlobs || !blackGlobs;
+    }
+
+    private boolean match(List<PathMatcher> globsMatcher, String path) {
+        return globsMatcher.stream()
+                .map(glob -> glob.matches(Path.of(path)))
+                .reduce(false, (aBoolean, aBoolean2) -> aBoolean || aBoolean2);
+    }
+}
